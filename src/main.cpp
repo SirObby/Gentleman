be78905215ae1c9eb8df99a8a4592f8796c122b6
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-10-14 13:54:28 +0800

    Added some args

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,39 @@ int main(int argc, char *argv[])
     if (argc != 1)
     {
 
+        if(v[1] == "build") {
+
+        }
+
+        if(v[1] == "project") {
+        
+        }
+
+        if(v[1] == "version") {
+            
+        }
+
+        if(v[1] == "help") {
+            if(v[2] == "build") {
+                cout << "usage: gent build <input>\n\nbuild compiles the source files you input.";
+            }
+            if(v[2] == "project") {
+                
+            }
+            if(v[2] == "version") {
+                cout << "usage: gent version\n\nList the version of the gentleman compiler.";
+            }
+        }
         
     }
     else
     {
-        cout << "Missing argument 0;";
+        cout << "gent is a tool for managing gentleman source code \n\nUsage:\n\n        gent <command> [<args>]\n\nThe commands are:\n\n"
+        << "        build       compile source code.\n"
+        << "        project     manage your gentleman project.\n"
+        << "        version     list the version of gentleman.\n"
+        << "\nUse \"gent help <command>\" for more information about a command."
+        ;
     }
 
     return 0;
